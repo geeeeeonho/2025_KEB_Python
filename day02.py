13d3546664daed588fdcd6b3195238b9e1ddeeef
이건호 <ggamea100@gmail.com>
2025-02-11 16:17:58 +0900

    v1.1) for -> while

--- day02.py
+++ day02.py
@@ -8,29 +8,40 @@
 # for i in range(1,10):
 #     print(f"{dan}*{i}={dan*i}")
 
-#입력 소수 확인하기(루트 범위로 줄이기)
-def is_prime(n) -> bool:    #1.함수생성
-    """
-   check prime number
-   if prime number return True
-   if not prime number return False
-    """
-    if n >= 2:
-        for i in range(2, int(n ** 0.5) + 1):  # n의 0.5승을 int로
-            if n % i == 0:
-                return False
-                break
-    else:
-        return False
-    return True
+# #입력 소수 확인하기(루트 범위로 줄이기)
+# def is_prime(n) -> bool:    #1.함수생성
+#     """
+#    check prime number
+#    if prime number return True
+#    if not prime number return False
+#     """
+#     if n >= 2:
+#         for i in range(2, int(n ** 0.5) + 1):  # n의 0.5승을 int로
+#             if n % i == 0:
+#                 return False
+#                 break
+#     else:
+#         return False
+#     return True
+#
+# n=int(input("input number :"))
+#
+# if is_prime(n):
+#     print(f"{n} is prime number.")
+# else:
+#     print(f"{n} is not prime number.")
+#
+# def help(is_prime):
+#     print('''소수를 판정하는 함수
+#     소수면 true를, 소수가 아니면 false를 리턴함''')
 
-n=int(input("input number :"))
+# format function
+dec = 65
+octal = 0o101
+hexadecimal = 0x41
+binary = 0b01000001
+print(dec, octal, hexadecimal, binary)
+print(chr(dec), chr(octal), chr(hexadecimal), chr(binary))
+print(bin(dec), bin(octal), bin(hexadecimal), bin(binary))
+print(ord('B'), ord('Z'), ord('a'), ord('2'))  # 66, 90, 97, 50
 
-if is_prime(n):
-    print(f"{n} is prime number.")
-else:
-    print(f"{n} is not prime number.")
-
-def help(is_prime):
-    print('''소수를 판정하는 함수
-    소수면 true를, 소수가 아니면 false를 리턴함''')
---
+++ day02_homework.py
@@ -0,0 +1,24 @@
+#1 for to while
+def is_prime(n) -> bool:    #1.함수생성
+    """
+   check prime number
+   if prime number return True
+   if not prime number return False
+    """
+    if n >= 2:
+        i=2
+        while i <=int(n ** 0.5) :
+            if n % i == 0:
+                return False
+                break
+            i+=1
+    else:
+        return False
+    return True
+
+n=int(input("input number :"))
+
+if is_prime(n):
+    print(f"{n} is prime number.")
+else:
+    print(f"{n} is not prime number.")
