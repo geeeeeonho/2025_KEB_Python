3248a1870aafaaf949b8cef166d917fa81db84d1
이건호 <ggamea100@gmail.com>
2025-02-11 10:34:06 +0900

    v2.0 99dan

--- day01.py
+++ day01.py
@@ -1,7 +1,3 @@
 name=input("what's your name?")
-<<<<<<< HEAD
-print(f"hello {name}")
-#shift+alt+f10 실행
-=======
-print(f"hello {name}") #shift+alt+f10 실행
->>>>>>> 052cb9794a514d53a64d542a5c4cccd5e5cb625f
+print(f"hello {name}") #ctrl+shift+f10
+
---
+++ day02.py
@@ -0,0 +1,10 @@
+#구구단
+# for dan in range(2,10,1):
+#     for i in range(1,10):
+#         print(f"{dan}*{i}={dan*1}")
+
+#입력받은 구구단만 출력
+dan = int(input("Input dan :"))
+for i in range(1,10):
+    print(f"{dan}*{i}={dan*i}")
+
