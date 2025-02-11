1faa10f4b22bfcfeeaa132a32157c39ce18f1c76
이건호 <ggamea100@gmail.com>
2025-02-11 11:39:31 +0900

    v0.7 prime number

--- day02.py
+++ day02.py
@@ -8,14 +8,19 @@
 # for i in range(1,10):
 #     print(f"{dan}*{i}={dan*i}")
 
-#소수 확인하기
-count = 0
-num=int(input("input number :"))
-for i in range(1,num+1):
-    if num%i==0:
-        count+=1
+#입력 소수 확인하기(루트 범위로 줄이기)
+n=int(input("input number :"))
+is_prime = True     #소수 판별 변수(참, 거짓)
 
-if count==2:
-    print(f"{num} is prime number.")
+if n>=2:
+    for i in range(2, int(n**0.5)+1):    #n의 0.5승을 int로
+        if n%i==0:
+            is_prime = False    #하나라도 나눠지면 거짓
+            break
 else:
-    print(f"{num} is not prime number.")
+    is_prime = False
+
+if is_prime:
+    print(f"{n} is prime number.")
+else:
+    print(f"{n} is not prime number.")
