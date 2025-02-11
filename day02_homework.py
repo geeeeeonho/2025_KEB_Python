cbda0f243082c05eb804475755c94405c03b00c0
이건호 <ggamea100@gmail.com>
2025-02-11 16:41:30 +0900

    v1.1) fix

--- day02_homework.py
+++ day02_homework.py
@@ -1,4 +1,3 @@
-#1 for to while
 def is_prime(n) -> bool:    #1.함수생성
     """
    check prime number
@@ -7,7 +6,7 @@ def is_prime(n) -> bool:    #1.함수생성
     """
     if n >= 2:
         i=2
-        while i <=int(n ** 0.5) :
+        while i <= (int(n ** 0.5) +1) :
             if n % i == 0:
                 return False
                 break
@@ -15,21 +14,8 @@ def is_prime(n) -> bool:    #1.함수생성
     else:
         return False
     return True
-
-
-def is_prime_print(n):
-    if is_prime(n):
-        print(n," ",end="")
-
-def range_prime(a,b):
-    while a <= b:   #최소를 최대가 될 때까지 반복 입력
-        is_prime(a)
-        is_prime_print(a)
-        a+=1
-    print("\nis prime numbers in range")
-
-num=input("range number(min max) :").split()    #띄어쓰기를 기준으로 구분
-nmin=int(num[0])    #앞을 최소
-nmax=int(num[1])    #뒤를 최대
-range_prime(nmin,nmax)
-
+n=int(input("input number :"))
+if is_prime(n):
+    print(f"{n} is prime number.")
+else:
+    print(f"{n} is not prime number.")
