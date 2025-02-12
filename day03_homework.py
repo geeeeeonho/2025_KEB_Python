6c0bf51c1a689ab0f07b47630c6f55f3c9ee1442
이건호 <ggamea100@gmail.com>
2025-02-12 17:36:53 +0900

    v0.2dayhomework

--- day03_homework.py
+++ day03_homework.py
@@ -9,17 +9,24 @@ df[0][1]="낙곱새"
 def recommand(n):    #메뉴 추천
     print(f'{df[n - 1][0]}에 어울리는 안주는 {df[n- 1][1]} 입니다')
 
+
+menu_list='다음 술 중에 고르세요.\n'
+i=0
+for i in range(1,len(df)+1):
+    menu_list=menu_list+f'{i}) {df[i-1][0]}   '
+menu_list=menu_list+f'{i+1}) 아무거나   {i+2}) 종료   :'
+
 while True:
     try:
-        menu = int(input(f'다음 술중에 고르세요.\n1) {df[0][0]}   2) {df[1][0]}   3) {df[2][0]}   4) {df[3][0]}   5) {df[4][0]}   6) 아무거나   7) 종료 : '))
+        menu = int(input(menu_list))    #입력받은 술 번호(menu)는 실제위치보다 1 많다.
     except:
         print("잘못된 입력입니다.")
         continue
-    if menu in range(1,6):
+    if menu in range(1,i+1):
         recommand(menu)
-    elif menu == 6:
+    elif menu == i+1:
         random_menu = random.randint(1,len(df))
         recommand(random_menu)
-    elif menu == 7:
+    elif menu == i+2:
         print(f'다음에 또 오세요')
         break
