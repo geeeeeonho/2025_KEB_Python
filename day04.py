d7c8b358ab9564cad56baa5b125af5f5c008d22c
이건호 <ggamea100@gmail.com>
2025-02-13 14:27:57 +0900

    v3.4 decorate

--- day04.py
+++ day04.py
@@ -1,14 +1,24 @@
 #개방 폐쇄의 원칙
 import time
 
+def time_decorator(func):
+    def wrapper(*arg):
+        s=time.time()
+        r=func(*arg)
+        e=time.time()
+        print(f'실행시간 : {e-s}초')
+        return r
+    return wrapper
+
+
+@time_decorator
 def fatorial_repetition(n):
     result=1
     for i in range(2,n+1):
         result=result*i
     return result
 
-num=int(input( ))
-s=time.time()
+num=int(input('! 구하기: ' ))
+
 print(f'{num}! = {fatorial_repetition(num)}')
-e=time.time()
-print(e-s)
+
