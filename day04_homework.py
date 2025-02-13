d311bdae30c6d939fa2fb8d5e64d7ca8deee6575
이건호 <ggamea100@gmail.com>
2025-02-13 17:45:49 +0900

    v4.2 day04 HW information

--- day04_homework.py
+++ day04_homework.py
@@ -3,13 +3,14 @@
 
 def log_decorator(func):
     def wrapper(*args, **kwargs):
+        # *변수는 위치 인수를 튜플로 저장
+        # **변수는 키워드 인수를 딕셔너리에 저장
         print(f'Function Name : {func.__name__}')
         print(f'Function Arguments : {args}')
         print(f'Function Keyword Arguments : {kwargs}')
         result = func(*args, **kwargs)
         return result
     return wrapper
-
 
 @log_decorator
 def greet(name, greeting="안녕하세요", age=0):
@@ -18,6 +19,5 @@ def greet(name, greeting="안녕하세요", age=0):
 
 print(greet("인하"))
 print(greet("인상", "안녕"))
-print(greet("James", "Hello"))
-print(greet("Gonzales", greeting="Hola"))
+print(greet("곤잘레스", greeting="홀라"))   #키워드 인수 키=벨류 형태
 print(greet("Nakamura", greeting="Gonniziwa", age=29))
